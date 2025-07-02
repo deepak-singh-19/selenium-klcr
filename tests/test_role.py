a75b5090d217e449ebd1c1b18cc5435fa53024a8
deepak-singh-19 <ds45063@gmail.com>
2025-07-02 22:05:50 +0530

    third commit

--- tests/test_role.py
+++ tests/test_role.py
@@ -4,13 +4,12 @@ from selenium.webdriver.common.by import By
 import time
 
 def test_open_role_page(driver):
-    #signinPage(driver).login("info@klcrinvestigations.com", "Insurance@123") #for staging
-    signinPage(driver).login("superadmin@insurance.com", "Insurance@123")   # for 67server
+    signinPage(driver).login("info@klcrinvestigations.com", "Insurance@123") #for staging
 
 
     # Click on left drawer button
-    driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[1]/button').click()
-    time.sleep(2)
+    # driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[1]/button').click()
+    # time.sleep(2)
 
     # Click System -> Role
     driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[2]/button').click()
