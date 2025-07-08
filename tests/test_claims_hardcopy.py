601f6c6f447771df4e07277be9026b7c3cbc58c6
deepak-singh-19 <ds45063@gmail.com>
2025-07-08 11:43:46 +0530

    claims module completed

--- tests/test_claims_hardcopy.py
+++ tests/test_claims_hardcopy.py
@@ -2,7 +2,7 @@ from pages.signin_page import signinPage
 import time
 from selenium.webdriver.common.by import By
 
-def test_open_case_management_page(driver):
+def test_open_hardcopy_page(driver):
     # ── 1. Sign in ─────────────────────────────────────────────
     signin = signinPage(driver)
     signin.login("info@klcrinvestigations.com", "Insurance@123")
@@ -11,12 +11,12 @@ def test_open_case_management_page(driver):
     driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[1]/button').click()
     time.sleep(2)
 
-    # Click Claims -> Case Management
+    # Click Claims -> Hard Copy
     driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[4]/button').click()
     time.sleep(1)
-    driver.find_element(By.XPATH,'/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[5]/div/div/div/div[1]/div[2]/span').click()
+    driver.find_element(By.XPATH,'/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[5]/div/div/div/div[3]/div[2]/span').click()
     time.sleep(2)
 
-    # Assert that the case management page is open
-    assert "case-management" in driver.current_url.lower()
-    print("Case Management page opened successfully")
+    # Assert that the hard copy page is open
+    assert "hard-copy" in driver.current_url.lower()
+    print("Hard Copy page opened successfully")
--- tests/test_claims_uploadclaims.py
+++ tests/test_claims_uploadclaims.py
@@ -2,7 +2,7 @@ from pages.signin_page import signinPage
 import time
 from selenium.webdriver.common.by import By
 
-def test_open_case_management_page(driver):
+def test_open_upload_claims_page(driver):
     # ── 1. Sign in ─────────────────────────────────────────────
     signin = signinPage(driver)
     signin.login("info@klcrinvestigations.com", "Insurance@123")
@@ -11,12 +11,12 @@ def test_open_case_management_page(driver):
     driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[1]/button').click()
     time.sleep(2)
 
-    # Click Claims -> Case Management
+    # Click Claims -> Upload Claims
     driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[4]/button').click()
     time.sleep(1)
-    driver.find_element(By.XPATH,'/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[5]/div/div/div/div[1]/div[2]/span').click()
+    driver.find_element(By.XPATH,'/html/body/div/div[2]/div/div[1]/div/div[2]/ul/div[5]/div/div/div/div[2]/div[2]/span').click()
     time.sleep(2)
 
-    # Assert that the case management page is open
-    assert "case-management" in driver.current_url.lower()
-    print("Case Management page opened successfully")
+    # Assert that the upload claims page is open
+    assert "upload-claims" in driver.current_url.lower()
+    print("Upload Claims page opened successfully")
